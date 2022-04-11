e56b23c6b4246ad99b823db262d2119cfb0f6715
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-04-11 13:49:20 -0700

    project structuring added, proper links to be added

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,7 +4,7 @@ import './AboutMe.css';
 const AboutMe = () => {
     return (
       <div className="aboutMeComponent">
-        <body className='aboutMeBody'><span className="first-letter">W</span>elcome to my portfolio!
+        <body className='aboutMeBody'><span className="first-letter">W</span>elcome!
         <br />
         <br />
         My name is Kae Saetern, pronounced kay SAY-turn. 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,18 +7,43 @@ import './Projects.css'
 const Projects = () => {
   return (
     <div className="projectsComponent">
-      <h1 className="title">Projects</h1>
+      <h2 className="title">Projects</h2>
+      
+      {/* Project 1 */}
       <div>1: Portfolio</div>
-      <img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" />
+      <br />
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="" /></a>
+      <a href="https://github.com/kaesae/Portfolio-Project"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
+      <br />
+      <br />
+      <br />
+      
+      {/* Project 2 */}
       <div>2: COVID-19 Statistics API</div>
-      <img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" />
-
+      <br />
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="" /></a>
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
+      <br />
+      <br />
+      <br />
+      
+      {/* Proejct 3 */}
       <div>3: Full-Stack Group Application (MERN)</div>
-      <img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" />
-
+      <br />
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="" /></a>
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
+      <br />
+      <br />
+      <br />
+      
+      {/* Project 4 */}
       <div>Project Description / Takeaways/ Group Work Flow / Conflicts</div>
       <div>4: Merit</div>
-      <img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" />
+      <br />
+      <a href=""><img className="little-logo" src="" /></a>
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
+      <a href="https://kaesae.github.io/Portfolio-Project/"><img className="little-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png" /></a>
     </div>
   );
 };

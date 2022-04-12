3c034768c7cdd0b1b4ef0605cb7d0c78beace6f9
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-04-12 12:31:08 -0700

    PreviousCareer component finished

--- src/Components/PreviousCareer/PreviousCareer.js
+++ src/Components/PreviousCareer/PreviousCareer.js
@@ -4,10 +4,30 @@ import './PreviousCareer.css';
 const PreviousCareer = () => {
     return (
       <div className="previousCareerComponent">
-        <body className='previousCareer'><span className="first-letter">W</span>elcome!
-        <br />
-        <br />
-        
+        <body className='previousCareerBody'>
+          {/* Paragraph 1 */}
+          <span className="previous-career-first-letter">C</span>urrently, I am the Founder and Owner of Valence LLC.
+          My knack for languages led me to take multiple language courses in college: 3 years of Spanish, 1 year of Japanese, and 1 year of American Sign Language.
+          In 2017, I began working with The Literacy and Language Center, LLC., a well-known private clinic that provided remedial-grade services, before graduating
+          with a BA in Communicative Disorders and Minor in Special Education in 2018.
+          <br />
+          <br />
+
+          {/* Paragraph 2 */}
+          <span className="previous-career-first-letter">W</span>hile applying my academic skills and knowledge to the beginning of my career of working with students
+          with academic delays from low-income households, I'd strongly and passionately argue that I have done some of the most fulfilling work I will ever do in my life.
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/ksBawxHK-0I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/FSNmOoh-KWg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          (Top video: Catch me playing basketball with a beloved student at 2:49! | Bottom video: My interview of part of the work we did.)
+          <br />
+          <br />
+
+          {/* Paragraph 3 */}
+          <span className="previous-career-first-letter">2</span> years, 2 promotions, and 4 job titles later, the clinic had to unfortunately close their doors.
+          I saw an opportunity and decided to start a business continuing the amazing work I had been doing. In April 2019, Valence LLC was formed and has been
+          going strong well into the pandemic. This year marks the 3-year anniversary of Valence's launch.
+          <br />
+          <br />
         </body>
       </div>
     );

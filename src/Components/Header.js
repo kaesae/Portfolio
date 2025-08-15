d3a0ae449ef125ae7a81f3b205609ef48062d78b
kaesae <kae.saetern@live.com>
2025-08-15 07:44:59 -0700

    navbar + animation added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,33 @@
-import PreviousCareer from "./PreviousCareer";
-import Projects from "./Projects";
 
 import { Link  } from "react-router-dom";
 
 
 const Header = () => {
+  
   return (
     <header>
       <ul>
-        <li>about</li>
-        <li>portfolio</li>
-        <li className="linkDiv"><Link className='links' to="/projects">projects</Link></li>
-        <li className="linkDiv"><Link className='links' to="/previous-career">previous career</Link></li>
-
+        <li 
+        className="linkDiv">
+          <Link 
+          className='links' 
+          to="/">
+            about
+            </Link></li>
+        <li 
+        className="linkDiv">
+          <Link 
+          className='links' 
+          to="/projects">
+            portfolio
+            </Link></li>
+        <li 
+        className="linkDiv">
+          <Link 
+          className='links' 
+          to="/previous-career">
+            previous career
+            </Link></li>
       </ul>
     </header>
   );
